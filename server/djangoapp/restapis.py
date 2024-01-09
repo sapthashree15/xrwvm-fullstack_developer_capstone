3bfb040bb81754a93a2d1913f19e9b8c018f3662
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2024-01-09 13:20:53 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,7 @@
 # Uncomment the imports below before you add the function code
 import requests
-import json
-from requests.auth import HTTPBasicAuth
+# import json
+# from requests.auth import HTTPBasicAuth
 
 import os
 from dotenv import load_dotenv
@@ -14,13 +14,13 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -32,10 +32,11 @@ def get_request(endpoint, **kwargs):
     except:
         # If any error occurs
         print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
+
 
 # def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -46,13 +47,16 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # def post_review(data_dict):
-# Add code for posting review
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
         print("Network exception occurred")
+    finally:
+        print("post_review call complete!")
+        
