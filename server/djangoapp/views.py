bcfab4e35cbff93bbfd8a93f27eee758515f8e58
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2024-01-09 14:47:32 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -129,7 +129,6 @@ def add_review(request):
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
         
-
 def get_cars(request):
     count = CarMake.objects.filter().count()
     if count == 0:
@@ -137,6 +136,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
-    
