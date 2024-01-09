b22304b0d3ec121162feb72866ca621941f5c306
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2024-01-09 16:18:42 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = \
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://sapthashreek-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+'https://sapthashreek-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://sapthashreek-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
