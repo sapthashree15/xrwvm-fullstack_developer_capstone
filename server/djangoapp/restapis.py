3892ba671386080c59112ee0ef7a8b542437dead
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2024-01-09 13:26:25 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,6 @@
 # Uncomment the imports below before you add the function code
 import requests
+
 # import json
 # from requests.auth import HTTPBasicAuth
 
@@ -8,28 +9,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
 
 # def get_request(endpoint, **kwargs):
 def get_request(endpoint, **kwargs):
     params = ""
-    if (kwargs):
+    if kwargs:
         for key, value in kwargs.items():
-            params = params+key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except: 
+    except:
         # If any error occurs
         print("Network exception occurred")
     finally:
@@ -38,7 +38,7 @@ def get_request(endpoint, **kwargs):
 
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -50,7 +50,7 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
@@ -59,4 +59,3 @@ def post_review(data_dict):
         print("Network exception occurred")
     finally:
         print("post_review call complete!")
-        
